052ad2269390b2cb21203844778f032237b8d7c7
marisa-edgar <marisaedgar1212@gmail.com>
2022-02-04 15:31:42 -0800

    add radio buttton inputs

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+import { User } from './src/js/main.js';
+
+let calculator = new User();
+
+$("form#formOne").submit(function(event) {
+  event.preventDefault();
+  let name = $("#name").val();
+  let age = parseInt($("#age").val());
+  let planet = $("input:radio[name=planet]:checked").val();
+}
